5d48dcce15e21a506c40fbaf2bf53c3082f7a202
VOLKANO-git <alaamim555@gmail.com>
2026-09-17 15:20:50 +0100

    message

--- conditions.js
+++ conditions.js
@@ -1,7 +1,8 @@
-let nombre = parseInt(prompt("Entrez un nombre entier : "), 10);
-console.log(nombre);arseInt(prompt("Entrez un nombre entier :"));
+const prompt = require('prompt-sync')();
+let nombre = Number(prompt("Entrez un nombre entier : "));
+console.log(nombre);
 if (nombre % 2 === 0) {
     console.log(`Le nombre ${nombre} est pair.`);
 } else {
-console.log(`Le nombre ${nombre} est impair.`);
+    console.log(`Le nombre ${nombre} est impair.`);
 } 
