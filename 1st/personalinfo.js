787cd880505a592035fb3d9552d346e8cc6fb350
VOLKANO-git <alaamim555@gmail.com>
2026-09-17 14:47:52 +0100

    suicide

--- 1st/personalinfo.js
+++
@@ -1,6 +0,0 @@
-const firstname = "Anas"
-const lastname = "Laamim"
-const age = 20 
-const gender = "Male"
-const email = "alaamim555@gmail.com"
-console.log(`My name is ${firstname}  ${lastname}. I am ${age} years old. I am a ${gender}. You can contact me at ${email}.`); 
---
+++ calculation.js
@@ -0,0 +1,3 @@
+const prompt = require('prompt sync');
+let a = Number(prompt("enter a number" ));
+let b = Number(prompt("enter a number"));
